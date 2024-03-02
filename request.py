f9e40ee5481a3f87c72d8b5e9c585dd9d15f8dc6
RaissaOliv <raissa130704@gmail.com>
2024-03-01 21:08:02 -0300

    redução do tratamento de input + polimento do request

--- request.py
+++ request.py
@@ -3,9 +3,16 @@ from inputTreatment import IsCnpjValid
 def RequestCnpj(cnpj):
     cnpj = IsCnpjValid(cnpj)
     if(cnpj):
-        r = requests.get(f'https://receitaws.com.br/v1/cnpj/{cnpj}')
-        if(r.status_code == 200):
-            print("STATUS 200: BUSCA REALIZADA COM SUCESSO\n\n")
-            print(r.text)
-        else:
-            raise Exception(f"ERRO {r.status_code}. A BUSCA NÃO PODE SER FEITA")
+        try:
+            r = requests.get(f'https://receitaws.com.br/v1/cnpj/{cnpj}')
+            result = r.json()
+            if(not isRequestValid):
+                print("STATUS 200: BUSCA REALIZADA COM SUCESSO\n\n")
+                print(result)
+                print(result['status'])
+            else:
+                print(f"ERRO DE REQUISIÇÃO. CNPJ NÃO ENCONTRADO")
+        except:
+            print(f"ERRO {r.status_code}. A BUSCA NÃO PODE SER FEITA")
+def isRequestValid(result):
+    return result['status'] == 'ERROR'
